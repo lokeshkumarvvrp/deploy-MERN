3f50545c7767e4fe789039ba3e457a447477ea0e
Lokeshkumarvvr <lokeshvvr17@gmail.com>
2024-06-02 17:04:54 +0530

    third commit

--- server/index.js
+++ server/index.js
@@ -29,14 +29,12 @@ app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors());
+app.use(cors({
+  origin: "https://deploy-mern-lwhq.vercel.app",
+  methods: ["POST", "GET"],
+  credentials: true
+}));
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
-
-app.use(cors(
-  origin: {"https://deploy-mern-lwhq.vercel.app"},
-  methods: ["POST","GET"],
-  Credentials: true
-))
 
 /* FILE STORAGE */
 const storage = multer.diskStorage({
